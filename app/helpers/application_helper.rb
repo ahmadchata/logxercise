3bab0fc8d8a3e46fc704b9e7347dec6b7dbbf691
Ahmad Chata <ahmadchata@gmail.com>
2021-01-14 00:18:00 +0100

    Add title per page basis

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Returns full title per page basis
+  def full_title(page_title= '')
+    base_title = "Logxercise"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
