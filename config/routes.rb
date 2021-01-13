b042d2a4bafb2446e4be3d2a442b421d5a2cda00
Ahmad Chata <ahmadchata@gmail.com>
2021-01-13 23:40:49 +0100

    Add header and footer in partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#new'
 end
