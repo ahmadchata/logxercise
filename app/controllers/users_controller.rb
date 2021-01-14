568c6118bffe8ece9a797eb21b83afda4a3e934f
Ahmad Chata <ahmadchata@gmail.com>
2021-01-14 19:38:40 +0100

    Add flash messages and update sign up redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Sign up successful"
+      redirect_to @user
     else
       render 'new'
     end
