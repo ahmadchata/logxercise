c2ba0a968407c480a50ac3c061db917157c6cbd0
Ahmad Chata <ahmadchata@gmail.com>
2021-01-19 18:15:30 +0100

    Add new login and logout to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'splash_screen/home'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'splash_screen#home'
   resources :users
