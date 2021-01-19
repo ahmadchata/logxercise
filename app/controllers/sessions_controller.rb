e25ef4fa639bfaa49d689663a9c73453eeeddd46
Ahmad Chata <ahmadchata@gmail.com>
2021-01-19 22:08:59 +0100

    Add create method to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+    if user&.authenticate(params[:session][:name])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid name'
+      render 'new'
+    end
+  end
 end
