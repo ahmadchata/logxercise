7be70aca534b3b468fe9d183f90654c2f064a957
Ahmad Chata <ahmadchata@gmail.com>
2021-01-26 18:03:34 +0100

    Add file upload field to new group form

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { maximum: 15 }
 end
