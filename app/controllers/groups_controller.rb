c1a662f7080522494919e8a64d5049f624955bdc
Ahmad Chata <ahmadchata@gmail.com>
2021-01-26 15:36:34 +0100

    Add create method to group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,12 +6,22 @@ class GroupsController < ApplicationController
   def new
   end
 
+  def create
+    @group = current_user.groups.build(group_params)
+    if @group.save
+      flash[:success] = "Group created"
+      redirect_to group_path
+    else
+      render 'group/new'
+    end
+  end
+
   def show
     @group = Group.find(params[:id])
   end
 
   private
-    def group_prams
+    def group_params
       params.require(:group).permit(:name)
     end
 end
