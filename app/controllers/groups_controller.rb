67f278f72808431bbf2459feb64ade48c70cac8b
Ahmad Chata <ahmadchata@gmail.com>
2021-01-26 17:00:54 +0100

    Add new form for creating group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,16 +1,18 @@
 class GroupsController < ApplicationController
+  before_action :require_login, only: %i[:create]
   def index
     @groups = Group.all
   end
 
   def new
+    @group = Group.new
   end
 
   def create
     @group = current_user.groups.build(group_params)
     if @group.save
       flash[:success] = "Group created"
-      redirect_to group_path
+      redirect_to groups_path
     else
       render 'group/new'
     end
