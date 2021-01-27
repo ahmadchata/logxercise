76ba6f826f5b5e94f182a5924fef9f3863b8bc07
Ahmad Chata <ahmadchata@gmail.com>
2021-01-27 10:32:41 +0100

    Add image resizing method to group model

--- Gemfile
+++ Gemfile
@@ -26,7 +26,8 @@ gem 'active_storage_validations', '0.8.2'
 # gem 'bcrypt', '~> 3.1.7'
 
 # Use Active Storage variant
-# gem 'image_processing', '~> 1.2'
+gem 'image_processing', '1.9.3'
+gem 'mini_magick', '4.9.5'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,9 @@ class Group < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 15 }
   validates :image, content_type: { in: %w[image/jpeg image/gif image/png], mesaage: "must be a valid image format" }, size: { less_than: 3.megabytes, message: "should be less than 3MB" }
+
+  # Returns a resized image
+  def display_image
+    image.variant(resize_to_limit: [500, 500])
+  end
 end
