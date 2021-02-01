a85b52e977185a001b3245cf6ef7f15143466fe3
Ahmad Chata <ahmadchata@gmail.com>
2021-02-01 13:32:18 +0100

    Remove group references from exercise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_144007) do
+ActiveRecord::Schema.define(version: 2021_02_01_121731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,10 +47,8 @@ ActiveRecord::Schema.define(version: 2021_01_27_144007) do
     t.string "name"
     t.integer "duration"
     t.bigint "user_id", null: false
-    t.bigint "group_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["group_id"], name: "index_exercises_on_group_id"
     t.index ["user_id"], name: "index_exercises_on_user_id"
   end
 
@@ -77,7 +75,6 @@ ActiveRecord::Schema.define(version: 2021_01_27_144007) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "exercises", "groups"
   add_foreign_key "exercises", "users"
   add_foreign_key "groups", "users"
 end
