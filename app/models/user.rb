7b3b8e462ba2ff7a467dc7a08c981b17fa915ad3
Ahmad Chata <ahmadchata@gmail.com>
2021-02-09 15:47:14 +0100

    Update validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :groups
   has_many :exercises
-  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 15 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 15 }
 end
