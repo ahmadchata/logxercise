7d14e197031e7c104488c3192c9f1e750420d34e
Ahmad Chata <ahmadchata@gmail.com>
2021-02-09 17:48:23 +0100

    Remove test folder

--- test/controllers/splash_screen_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class SplashScreenControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get splash_screen_home_url
-    assert_response :success
-  end
-end
