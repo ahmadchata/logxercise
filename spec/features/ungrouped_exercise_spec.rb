8b8595526f925083c92e9249b28d5d6b92e126f0
Ahmad Chata <ahmadchata@gmail.com>
2021-02-10 22:12:13 +0100

    Add feature test for ungrouped exercises

---
+++ spec/features/ungrouped_exercise_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'View ungrouped exercise', type: :feature do
+  before :each do
+    @user = User.create!(name: "Ahmad")
+  end
+  it 'visits the ungrouped exercises' do
+    visit '/login'
+    fill_in 'Name', with: "Ahmad"
+    click_button 'Log in'
+    click_link 'All my external exercises'
+    expect(page).to have_content("External Exercises")
+  end
+end
