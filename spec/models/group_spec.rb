447e7d90228ef598e180a5eea35de3a08adb9068
Ahmad Chata <ahmadchata@gmail.com>
2021-02-10 17:18:49 +0100

    Add association to group model test

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -30,7 +30,26 @@ RSpec.describe Group, type: :model do
   context 'Validates length of group name' do
     it 'should be valid' do
       @group = Group.create(name: "Cardial", user_id: user.id)
+      expect(@group.save).to be true
+    end
+    it 'should not be too short' do
+      @group = Group.create(name: "Ca", user_id: user.id)
+      expect(@group.save).not_to be true
+    end
+    it 'should not be too long' do
+      @group = Group.create(name: "C" * 16, user_id: user.id)
+      expect(@group.save).not_to be true
     end
   end
 
+  context 'Association between group and exercise model' do
+    it 'assign many exercises to a group' do
+      @group = Group.create!(name: "Aerobic", user_id: user.id)
+      @exercise1 = Exercise.create!(name: "Push ups", duration: 1, user_id: user.id)
+      @exercise2 = Exercise.create!(name: "Jumping", duration: 2, user_id: user.id)
+      @group.exercises << @exercise1
+      @group.exercises << @exercise2
+      expect(@group.exercises.last.name).to eql("Jumping")
+    end
+  end
 end
