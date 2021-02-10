35f0b5bdcebb03614695b16672ecf4847213435d
Ahmad Chata <ahmadchata@gmail.com>
2021-02-10 21:21:15 +0100

    Add feature test for group creation

---
+++ spec/features/create_group_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'New Group', type: :feature do
+  before :each do
+    @user = User.create!(name: "Ahmad")
+  end
+  it 'creates a new group' do
+    visit '/login'
+    fill_in 'Name', with: @user.name
+    click_button 'Log in'
+    expect(current_path).to eql("/users/1")
+
+    click_link 'All groups'
+    click_link 'Create new group'
+
+    fill_in 'Name', with: "Cardial"
+
+    click_button 'Create Group'
+    expect(page).to have_content("Cardial")
+  end
+end
